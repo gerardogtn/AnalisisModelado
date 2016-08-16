bbb34b5c736359a0bcf8de78819352ed930b98bb
Gerardo Teruel <gerardogtn@gmail.com>
2016-08-16 16:24:51 -0500

    Remove casts

--- src/tarea02/main.cpp
+++ src/tarea02/main.cpp
@@ -8,10 +8,10 @@
 
 int main(int argc, char const *argv[]) {
   ComputerFactory* factory = ComputerFactory::getInstance();
-  Laptop * l = (Laptop *) factory->create("laptop");
-  Desktop * d = (Desktop *) factory->create("desktop");
-  Netbook * n = (Netbook *) factory->create("netbook");
-  Tablet * t = (Tablet *) factory->create("tablet");
+  AbstractComputer * l = factory->create("laptop");
+  AbstractComputer * d = factory->create("desktop");
+  AbstractComputer * n = factory->create("netbook");
+  AbstractComputer * t = factory->create("tablet");
   std::cout << l->describe() << std::endl;
   std::cout << d->describe() << std::endl;
   std::cout << n->describe() << std::endl;
